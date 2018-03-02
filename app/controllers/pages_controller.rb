441b448df6d215528411c6b42e4dc00223e004d6
Daniel <dordarus@gmail.com>
2018-03-02 13:56:00 +0200

    Fixing bug with displaing user's page after stripe adding

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
     respond_to :html, :json
     
     def index
+        if user_signed_in?
+            redirect_to new_charge_path
+        else
+            render :index
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {sessions: 'users/sessions', omniauth_callbacks: 'auth/callbacks' }
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations', omniauth_callbacks: 'auth/callbacks' }
   resources :users
   resources :charges, only: [:new, :create]
   get 'thanks', to: 'charges#thanks', as: 'thanks'
-  root "charges#new"
+  root "pages#index"
 end
